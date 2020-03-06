ef403c96dd62b10cd58be39afa04de1614834682
firmboy <yangjian5268@gmail.com>
2020-03-06 14:20:33 +0800

    测试git提交

---
+++ src/main/java/com/firmboy/search/account/vo/AccountContext.java
@@ -0,0 +1,13 @@
+package com.firmboy.search.account.vo;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author playboy
+ * @create 2020-03-06 2:19 下午
+ * @description 入账的上下文
+ * @serviceLogic
+ **/
+public class AccountContext {
+    private Logger log = Logger.getLogger(AccountContext.class);
+}
